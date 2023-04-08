c6e39b1789c2d2b957d2ba5b48566db311bb72c8
BHAVYAPODDAR <poddarbhavya3@gmail.com>
2023-04-08 16:26:49 +0530

    krisha added doctors page

--- frontend/src/pages/Doctors/Doctors.js
+++ frontend/src/pages/Doctors/Doctors.js
@@ -1,22 +1,64 @@
-import React from 'react'
-import {BsSearch} from 'react-icons/bs'
-import Sidebar from '../Appointment/Sidebar'
-import ImageCard from '../../components/Imagecard'
+import React, { useState } from 'react'
+import Navbar from './Navbar';
+import Menu from './DoctorApi';
+import DoctorCard from './DoctorCard';
+import { Link } from 'react-router-dom'
+import { MdArrowBack } from 'react-icons/md'
+import Video from './Video';
+import Chat from './Chat';
+import './style.css';
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
 
 const Doctors = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
 
-  // const doctorsInfo = [
-  //   {
-  //     image: 
-  //   },
-  // ]
+  
 
-  return (
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+
     
-    <div>
-      Doctors
-    </div>
-  )
-}
+  return (
+    <>
+    <div className='bg-gradient-to-br from-[#e8f2fc] to-white h-screen w-full'>
+      <div className='left-2 mt-4'>
+                    
+                        <button>
+                            <Link to='/'>
+                            <MdArrowBack className='w-5 h-5' />
+
+                            </Link>
+                        </button>
+                    
+      </div>
+          <Navbar filterItem={filterItem} menuList={menuList} />
+
+      <DoctorCard menuData={menuData} />
+      <Chat />
+      <Video />
+      </div>
+      
+    </>
+  );
+};
 
 export default Doctors
